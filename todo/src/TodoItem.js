42dce71e121fd68c212599b9a9a0b1f982c16efe
streloc84 <streloc84@gmail.com>
2020-01-11 05:48:49 +0400

    events

--- todo/src/TodoItem.js
+++ todo/src/TodoItem.js
@@ -2,8 +2,9 @@ import React from "react"
 function TodoItem(props) {
 return (
 <div className="todo-item">
-<input type="checkbox" checked={props.item.completed}/>
+<input type="checkbox" checked={props.item.completed} onChange={()=>{console.log("Нажал")}}/>
 <p>{props.item.text}</p>
+
 </div>
 )
 }
