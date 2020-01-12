e88584b9ac2f541ce7b29c5a3f32cccd08dca426
streloc84 <streloc84@gmail.com>
2020-01-12 04:24:56 +0400

    todo with state

--- todo/src/TodoItem.js
+++ todo/src/TodoItem.js
@@ -1,5 +1,41 @@
 import React from "react"
-function TodoItem(props) {
+
+class TodoItem extends React.Component{
+    constructor(){
+        super()
+        this.state = {
+        count: 0
+        }
+        this.handleClick = this.handleClick.bind(this)
+    }
+
+    handleClick() {
+        this.setState(prevState => {
+        return {
+        count: prevState.count + 1
+        }
+        })
+        }
+        
+    render(){
+     
+            
+            
+        return (
+            <div className="todo-item">
+            <input type="checkbox" checked={(this.state.count%2)?true:false} onChange={this.handleClick}/>
+            <p>{this.props.item.text}</p>
+            
+            </div>
+            )
+    }
+}
+
+
+
+
+
+/* function TodoItem(props) {
 return (
 <div className="todo-item">
 <input type="checkbox" checked={props.item.completed} onChange={()=>{console.log("Нажал")}}/>
@@ -7,5 +43,5 @@ return (
 
 </div>
 )
-}
+} */
 export default TodoItem
